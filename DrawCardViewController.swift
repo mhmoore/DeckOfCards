7927127752f56eeffefeb82cca2a87dac7efe229
Mike Moore <michaelhmoore1@gmail.com>
2019-08-12 11:01:22 -0600

    added MVC files

---
+++ DrawCardViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DrawCardViewController.swift
+//  DeckOfCards
+//
+//  Created by Michael Moore on 8/12/19.
+//  Copyright © 2019 Michael Moore. All rights reserved.
+//
+
+import UIKit
+
+class DrawCardViewController: UIViewController {
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var suitLabel: UILabel!
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func drawButtonTapped(_ sender: Any) {
+    }
+    
+}
